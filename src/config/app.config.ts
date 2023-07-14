90def834d6866da70b1236235634f4cdf4eb5ec8
kellyncodes <kellyncodes@gmail.com>
2023-07-14 17:28:08 +0100

    chore: appConfig URI setting

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,6 @@
 export const appConfig = {
-  //serverURL: 'https://profiler.up.railway.app',
-  serverURL: 'http://localhost:5000',
-  clientURL: 'http://localhost:4200',
+  serverURL: 'https://profiler.up.railway.app',
+  clientURL: 'https://profiler.ui.netlify.app',
+  //serverURL: 'http://localhost:5000',
+  //clientURL: 'http://localhost:4200',
 };
